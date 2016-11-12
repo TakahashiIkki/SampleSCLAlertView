f14cecc4b68be57c9a97784f27509aced521744a
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-11-12 11:51:02 +0900

    Sample SCLAlertView Source

--- SampleSCLAlertView/ViewController.swift
+++ SampleSCLAlertView/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func showErrorAlert(_ sender: Any) {
+        SCLAlertView().showError("Sample Error", subTitle: "Error内容を表示する。") // Error
+    }
 
+    @IBAction func showNoticeAlert(_ sender: Any) {
+        SCLAlertView().showNotice("Sample Notice", subTitle: "Notice内容を通知する。") // Notice
+    }
+    
+    @IBAction func showWarningAlert(_ sender: Any) {
+        SCLAlertView().showWarning("Sample Warning", subTitle: "Warning内容を通知する。") // Warning
+    }
+
+    @IBAction func showInfoAlert(_ sender: Any) {
+        SCLAlertView().showInfo("Sample Info", subTitle: "Info内容を通知する。") // Info
+    }
+    
+    @IBAction func showEditAlert(_ sender: Any) {
+        SCLAlertView().showEdit("Sample Edit", subTitle: "Edit内容を通知する。") // Edit
+    }
+    
+    @IBAction func hideCloseButtonAlert(_ sender: Any) {
+        let appearance = SCLAlertView.SCLAppearance(
+            showCloseButton: false
+        )
+        let alertView = SCLAlertView(appearance: appearance)
+        alertView.showSuccess("Sample No button", subTitle: "2秒後に消えます", duration: 2)
+        
+        // durationを指定しなかったら、永久に残る。。。
+    }
+    
+    
 }
 
